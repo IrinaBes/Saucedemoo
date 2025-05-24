061bae8e26892610c6d583580e72f0c423ab4379
IrinaBes <iri311083@gmail.com>
2025-05-24 21:56:02 +0400

    New test classes and allure connection is added

---
+++ src/test/java/tests/AddGoodsToCartTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+import static user.UserFactory.withAdminPermission;
+
+public class AddGoodsToCartTest extends BaseTest {
+    @Epic("Модуль логина интернет-магазина")
+    @Feature("Юридические лица")
+    @Story("STG")
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("Savinova Irina iri311083@gmail.com")
+    @Test(description = "Проверяем, что товары добавлены в корзину")
+    public void checkGoodsInCart() {
+        //productsPage.addToCart("Sauce Labs Backpack");
+        LoginPage.open();
+        LoginPage.Login(withAdminPermission());
+        productsPage.isOpen();
+        productsPage.addToCart(0);
+        productsPage.addToCart(2);
+        productsPage.addToCart(3);
+        productsPage.openCart();
+        assertTrue(cartPage.getProductsNames().contains("Sauce Labs Backpack"));
+        assertEquals(cartPage.getProductsNames().size(), 3);
+        assertFalse(cartPage.getProductsNames().isEmpty());
+    }
+}
---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductsPage;
+import utils.PropertyReader;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    public WebDriver driver;
+    LoginPage LoginPage;
+    ProductsPage productsPage;
+    CartPage cartPage;
+    String user;
+    String password;
+
+    @Parameters({"browser"})
+    @BeforeMethod
+    @Step()
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            options.addArguments("--guest");
+            //options.addArguments("headless");
+            driver = new ChromeDriver(options);
+        }
+        else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait( 6, TimeUnit.SECONDS);
+        LoginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        cartPage = new CartPage(driver);
+
+        System.setProperty("BASE_URL", PropertyReader.getProperty("saucedemoo.url"));
+        user = PropertyReader.getProperty("saucedemoo.user");
+        password = PropertyReader.getProperty("saucedemoo.password");
+
+    }
+
+    @Step("Закрытие")
+    @AfterMethod()
+    public void close() {
+    //    driver.quit();
+    }
+}
